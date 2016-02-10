3f702de0762a14725a500e11c9ec6b8ed2b07b76
rsdpisuy <rsdpisuy@gmail.com>
2016-02-10 16:13:06 +0300

    Comment AssemblyInfo attributes which should be generated automatically.

--- Documents/AIV.cs
+++ Documents/AIV.cs
@@ -1,12 +1,10 @@
 using System.Reflection;
 
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("panthernet dev works")]
-[assembly: AssemblyProduct("GraphX for .NET")]
 [assembly: AssemblyCopyright("Copyright www.panthernet.ru ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.3.3.0")]
-[assembly: AssemblyFileVersion("2.3.3.0")]
+/*[assembly: AssemblyVersion("2.3.3.0")]
+[assembly: AssemblyFileVersion("2.3.3.0")]*/
