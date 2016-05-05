f0796a9f8e9cf739e625e99cec22846e046e07bb
Eugene Auduchinok <eugene.auduchinok@gmail.com>
2016-05-05 23:43:03 +0300

    Another assembly info fix

--- GraphX.Controls/Properties/AssemblyInfo.cs
+++ GraphX.Controls/Properties/AssemblyInfo.cs
@@ -3,17 +3,6 @@ using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Markup;
 
-[assembly: AssemblyTitleAttribute("GraphX.WPF.Controls")]
-[assembly: AssemblyProductAttribute("QuickGraph")]
-[assembly: AssemblyDescriptionAttribute("Graph datastructures and algorithms for .NET.")]
-[assembly: AssemblyVersionAttribute("0.0.1")]
-[assembly: AssemblyFileVersionAttribute("0.0.1")]
-namespace System {
-    internal static class AssemblyVersionInformation {
-        internal const string Version = "0.0.1";
-    }
-}
-
 [assembly: XmlnsPrefix("http://schemas.panthernet.ru/graphx/", "graphx")]
 [assembly: XmlnsDefinition("http://schemas.panthernet.ru/graphx/",
     "GraphX.Controls")]
@@ -25,4 +14,17 @@ namespace System {
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None,
     ResourceDictionaryLocation.SourceAssembly
-)] 
+)]
+
+[assembly: AssemblyTitleAttribute("GraphX.WPF.Controls")]
+[assembly: AssemblyProductAttribute("QuickGraph")]
+[assembly: AssemblyDescriptionAttribute("Graph datastructures and algorithms for .NET.")]
+[assembly: AssemblyVersionAttribute("0.0.1")]
+[assembly: AssemblyFileVersionAttribute("0.0.1")]
+namespace System {
+    internal static class AssemblyVersionInformation {
+        internal const string Version = "0.0.1";
+    }
+}
+
+
